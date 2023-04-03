f795893df2629236713ef5e2c25c0818c6bbf60c
janarthanan09 <janajana26222@gmail.com>
2023-04-03 17:29:17 +0530

    new store

---
+++ src/org/test/Large.java
@@ -0,0 +1,19 @@
+package org.test;
+
+public class Large {
+
+	public static void main(String[] args) {
+		int a=9,b=23,c=923;
+		int larg = a>b?a:b;
+				System.out.println(larg);
+				
+				int num= larg>c? larg:c;
+				
+				System.out.println(num);
+		
+				
+	}
+	
+	
+	
+}
